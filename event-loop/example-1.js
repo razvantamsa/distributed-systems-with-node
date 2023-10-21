ec25b4bd69411c54ca8f2ad97bc9f528dc8d2fc4
Razvan Tamsa <razvan.tamsa@opswat.com>
2023-10-21 16:01:24 +0300

    event-loop example 1

---
+++ event-loop/example-1.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+setImmediate(() => console.log(1));
+Promise.resolve().then(() => console.log(2));
+process.nextTick(() => console.log(3));
+fs.readFile(__filename, () => {
+    console.log(4);
+    setTimeout(() => console.log(5));
+    setImmediate(() => console.log(6));
+    process.nextTick(() => console.log(7));
+});
+console.log(8);
+
+// my prediction:
+8
+3
+1
+2
+4
+7
+6
+5
