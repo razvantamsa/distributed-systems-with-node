f8cda1be9f142d49e9e8c40e99fc5846aacfaf0f
Razvan Tamsa <razvan.tamsa@opswat.com>
2023-11-06 15:10:22 +0200

    add utils folder

---
+++ local-packages/utils/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    sum(a, b) {
+        return a+b;
+    },
+    
+    diff(a, b) {
+        return a-b;
+    },
+    
+    product(a, b) {
+        return a*b;
+    },
+    
+    subract(a, b) {
+        return a/b;
+    }
+}
